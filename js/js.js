d2e250dd8291bf950918f9331f8233f6502af9df
whj11200 <74046757+whj11200@users.noreply.github.com>
2023-06-11 21:15:55 +0900

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,16 @@
+$('.gallery').slick({
+    infinite: true,
+    dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    pauseOnHover: false,
+    speed: 2000,
+    cssEase: 'linear',
+    pauseOnDotsHover: false,
+    pauseOnFocus: false,
+    waitForAnimate: false,
+    swipe: true,
+    draggable: true
+});
